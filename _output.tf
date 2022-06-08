c04a62814a91430591620301d22491be7589817e
José Roberto <jrpradojunior@gmail.com>
2022-06-07 22:34:16 -0300

    Added option to store sensitive data in ssm or secret manager

--- _output.tf
+++ _output.tf
@@ -2,7 +2,7 @@ output "redis_endpoint" {
   value = aws_elasticache_replication_group.redis.primary_endpoint_address
 }
 
-output "id" {
+output "redis_id" {
   value = aws_elasticache_replication_group.redis.id
 }
 
